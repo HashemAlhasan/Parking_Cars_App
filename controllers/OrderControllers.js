d759f17d9ec5e2aeeb53018fc6f3a429730e991e
hashemalhasan <eng.hashem.alhasan@gmail.com>
2024-06-18 12:42:25 +0300

    edits

--- controllers/OrderControllers.js
+++ controllers/OrderControllers.js
@@ -9,8 +9,9 @@ try {
         res.status(StatusCodes.BAD_REQUEST).json({message:"Please Provide User Name"})
     }
     const  user = await User.findOne({username:username})
+    console.log(user);
     if(!user){
-        res.status(StatusCodes.BAD_REQUEST).json({message: "User Not Found"})
+        return res.status(StatusCodes.BAD_REQUEST).json({message: "User Not Found"})
     }
     const orders = await ParkingOrder.find({userId:user._id}).sort({createdAt:-1}).
     populate('userId','email firstName lastName').populate('SelectedPark','location.Price location.parkingName')
@@ -42,13 +43,61 @@ export const getAllRepairOrders = async(req,res)=>{
     if(!orders){
         res.status(StatusCodes.BAD_REQUEST).json({message :"No Orders For This User"})
     }
+    console.log(orders);
 
- return   res.status(StatusCodes.ACCEPTED).json(orders)
+ return   res.status(StatusCodes.OK).json(orders)
         
     } catch (error) {
         console.log(error);
     return res.status(StatusCodes.BAD_REQUEST).json({message :error})
         
     }
+
+}
+export const DeleteOrderParking = async(req,res)=>{
+    try {
+        const {id}=req.body 
+  
+    if(!id){
+        return res.status(StatusCodes.BAD_REQUEST).json({message:"please Provide Order Id"})
+    }
+    const order= await ParkingOrder.findByIdAndDelete(id)
+    console.log(order);
+    if(!order){
+        return res.status(StatusCodes.BAD_REQUEST).json({message :"Order Not Found "})
+
+    }
+
+    return res.status(StatusCodes.OK).json({message :"Deleted Sucesffuly"})
+
+        
+    } catch (error) {
+        return res.status(StatusCodes.BAD_REQUEST).json({messgae:error})
+        
+    }
+    
+}
+export const DeleteOrderRepair = async(req,res)=>{
+    try {const {id:orderId}=req.body
+    console.log(orderId);
+    if(!orderId){
+        return res.status(StatusCodes.BAD_REQUEST).json({message:"please Provide Order Id"})
+    }
+    const order= await RepairOrder.findOneAndDelete({_id:orderId})
+    console.log(order);
+    if(!order){
+        return res.status(StatusCodes.BAD_REQUEST).json({message :"Order Not Found "})
+
+    }
+
+
+    return res.status(StatusCodes.OK).json({message :"Deleted Sucesffuly"})
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(StatusCodes.BAD_REQUEST).json({message :"Error"})
+        
+    }
+    
 
 }
